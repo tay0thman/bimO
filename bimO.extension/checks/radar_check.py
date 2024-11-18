6f6ae45b1faeedba3c245d6cb412ea34a8cbfd23
tay.0 <tay0thman@users.noreply.github.com>
2024-11-18 02:17:36 -0800

    Fixes per feedback

--- bimO.extension/checks/radar_check.py
+++ bimO.extension/checks/radar_check.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
-# region Imports
+
 import math
 from pyrevit import revit, DB, DOCS, HOST_APP
 from pyrevit import script
 from pyrevit.preflight import PreflightTestCase
-# endregion
-# region Constants
+
 doc = DOCS.doc  # Current Document
 output = script.get_output()
-divider = "_" * 200
-test_score = 0
+divider = "_" * 120
 INTERNAL_ORIGIN = (0, 0, 0)
 EXTENT_DISTANCE = 52800  # Linear Feet
 BAD_STRG = '### :thumbs_down_medium_skin_tone: ............'
 GOOD_STRG = '### :OK_hand_medium_skin_tone: ............'
 WARN_STRG = '### :warning: ............'
 CRITERIA_STRG = '10 Mi (16KM) away from the Internal Origin.'
-# endregion
-# region Functions
-def class_collectors(document=doc, class_type=None, toggle=False):
+
+
+def class_collectors(document=doc, class_type=None):
     """
     Collects all elements of a specified class type.
     Args:
@@ -28,18 +26,17 @@ def class_collectors(document=doc, class_type=None, toggle=False):
     Returns:
         elements (list): A list of elements of the specified class type.
     """
-    if toggle:
-        elements = (DB.FilteredElementCollector(document).
+    if class_type:
+        return (DB.FilteredElementCollector(document).
                 WhereElementIsNotElementType().
                 OfClass(class_type).
                 ToElementIds())
     else:
-        elements = (DB.FilteredElementCollector(document).
+        return (DB.FilteredElementCollector(document).
                 WhereElementIsNotElementType.
                 ToElementIds())
-    return elements
 
-def make_temp_view(document=doc, object=None):
+def make_temp_view(document=doc):
     """
     Creates a temporary 3D view with the specified name.
     Args:
@@ -52,8 +49,9 @@ def make_temp_view(document=doc, object=None):
                      .OfClass(DB.ViewFamilyType)
                      .WhereElementIsElementType()
                      .ToElements())
-    three_d_view_type = ([v for v in view_3D_types 
-                          if v.ViewFamily == DB.ViewFamily.ThreeDimensional][0])
+    three_d_view_type = next(
+        v for v in view_3D_types if v.ViewFamily == DB.ViewFamily.ThreeDimensional
+            )
     
     view = DB.View3D.CreateIsometric(document, three_d_view_type.Id)
     worksets = (DB.FilteredWorksetCollector(document).
@@ -61,20 +59,25 @@ def make_temp_view(document=doc, object=None):
                 ToWorksets())
     for ws in worksets:
         view.SetWorksetVisibility(ws.Id, DB.WorksetVisibility.Visible)
+    view.IsSectionBoxActive = False
     view.IsSectionBoxActive = True
-    if object is not None:
-             view.IsolateElementTemporary(object.Id)
     return view
 
-def get_temp_view_bbox(document=doc):
+def get_temp_view_bbox(document=doc, element=None):
     """
     Gets the bounding box of the 3D view.
     Args:
         doc (Document): The current document.
+        element (Element): The element to isolate in the temporary view.
     Returns:
         bbox (BoundingBoxXYZ): The bounding box of the 3D view."""
     view = make_temp_view(document)
+    view.IsSectionBoxActive = False
+    view.IsSectionBoxActive = True
+    if element:
+        view.IsolateElementTemporary(element.Id)
     bbox = view.GetSectionBox()
+
     return bbox
 
 def hide_linked_elements(document=doc, 
@@ -108,7 +111,7 @@ def get_all_cads(document=doc):
     Returns:
         cads (list): A list of CAD Links in the model.
         """
-    cads = class_collectors(document, DB.ImportInstance, True)
+    cads = class_collectors(document, DB.ImportInstance)
     return cads
 
 def get_all_rvts(document=doc):
@@ -119,10 +122,10 @@ def get_all_rvts(document=doc):
     Returns:
         rvts (list): A list of Revit Links in the model.
         """
-    rvts = class_collectors(document, DB.RevitLinkInstance, True)
+    rvts = class_collectors(document, DB.RevitLinkInstance)
     return rvts
 
-def get_far_elements(document=doc, cad=False, rvt=False, others=False):
+def get_far_elements(document=doc, cad=False, rvt=False):
     """
     Gets all elements that are more than 10 miles from the internal origin.
     Args:
@@ -138,22 +141,28 @@ def get_far_elements(document=doc, cad=False, rvt=False, others=False):
     temp_view = make_temp_view(document)
     if cad:
         elementsids = (get_all_cads(document))
-        temp_view.IsolateElementsTemporary(elementsids)
     elif rvt:
         elementsids = (get_all_rvts(document))
-        temp_view.IsolateElementsTemporary(elementsids)
-    elif others:
+    elif not cad and not rvt:
         hide_linked_elements(document,
                         temp_view,
                         hide_cads=True,
                         hide_rvts=True)
+        temp_view.IsSectionBoxActive = True
+        temp_view.IsSectionBoxActive = False
         elementsids = (DB.FilteredElementCollector(document, temp_view.Id).
                         WhereElementIsNotElementType().
                         ToElementIds())
     for elementid in elementsids:
         element = document.GetElement(elementid)
-        if ((element.get_BoundingBox(temp_view)) and
-             (hasattr(element, 'Name') and hasattr(element, 'Category'))):
+        if ((element.get_BoundingBox(temp_view)) 
+            and (hasattr(element, 'Name') 
+            and  hasattr(element, 'Category')) 
+            and ("Section Box" not in element.Name) 
+            and ("3D View" not in element.Name)
+            and (element.Category.Name != "Cameras") 
+            and (element.Category.Name != "Levels") 
+            and (element.Category.Name != "Grids")):
             bbox = element.get_BoundingBox(temp_view)
             if (check_bounding_box(bbox, INTERNAL_ORIGIN, EXTENT_DISTANCE)
                     == 0):
@@ -256,6 +265,26 @@ def calculate_angle(point1, point2):
     angle = round(math.degrees(math.atan2(y2 - y1, x2 - x1)), 2)
     return angle
 
+def points_distances_angles(point):
+    """
+    Calculates the distances and angles between the internal origin and a point.
+    Args:
+        point (tuple): The coordinates of the point.
+    Returns:
+        distances (tuple): The distances between the internal origin and the point.
+        angles (tuple): The angles between the internal origin and the point.
+        x (string): The unit conversion value x-coordinate of the point.
+        y (string): The unit conversion value y-coordinate of the point.
+        z (string): The unit conversion value z-coordinate of the point.
+    """
+    x, y, z = point
+    x = convert_values(x)
+    y = convert_values(y)
+    z = convert_values(z)
+    distances = convert_units(abs(calculate_distance(point, INTERNAL_ORIGIN)))
+    angles = convert_units(calculate_angle(point, INTERNAL_ORIGIN))
+    return x, y, z, distances, angles
+
 def check_bounding_box(bbox, intorig, extentdistance):
     """
     Checks if the domain of the bounding box is more than 10
@@ -333,8 +362,7 @@ def get_design_options_elements(document=doc):
                 ToElements())
         design_option_elements.append(x)
     return design_options, design_option_elements, design_option_sets
-# endregion
-# region Main Output Functions
+
 def check_model_coordinates(document=doc):
     """
     Checks and reports the coordinates of the project base and survey points.
@@ -359,50 +387,50 @@ def check_model_coordinates(document=doc):
     basptdistance = abs(calculate_distance(baspt, INTERNAL_ORIGIN))
     surveydistance = abs(calculate_distance(survpt, INTERNAL_ORIGIN))
 
-    if basptdistance > EXTENT_DISTANCE:
+    if abs(basptdistance > EXTENT_DISTANCE):
         output.print_md('{} Base Point is more than {}'.format(BAD_STRG, CRITERIA_STRG))
-    if surveydistance > EXTENT_DISTANCE:
+    if abs(surveydistance > EXTENT_DISTANCE):
         output.print_md('{} Survey Point is more than {}'.format(BAD_STRG, CRITERIA_STRG))
     else:
         output.print_md('{} Survey Point is less than {}'.format(GOOD_STRG, CRITERIA_STRG))
     basptdistance = calculate_distance(baspt, INTERNAL_ORIGIN)
     # ____________________________________Calculate the angle between the points
-    baseptangle = calculate_angle(baspt, INTERNAL_ORIGIN)
-    surveyptangle = calculate_angle(survpt, INTERNAL_ORIGIN)
     truenorthangle = calculate_angle(baspt, survpt)
     # _______________________________Print the Project Coordinates and Distances
     tbdata = [['Internal Origin Coordinates', 
         str(INTERNAL_ORIGIN[0]),str(INTERNAL_ORIGIN[1]),str(INTERNAL_ORIGIN[2]),
         ' ', ' '],
         ['Project Base Point Coordinates to Internal Origin', 
-        str(convert_values(baspt[0],doc)),
-        str(convert_values(baspt[1],doc)),
-        str(convert_values(baspt[2], doc)), 
-        str(convert_units(basptdistance, document)), baseptangle],
+            points_distances_angles(baspt)[0],
+            points_distances_angles(baspt)[1],
+            points_distances_angles(baspt)[2],
+            points_distances_angles(baspt)[3],
+            points_distances_angles(baspt)[4]],
         ['Survey Point Coordinates to Internal Origin', 
-        str(convert_values(survpt[0], doc)),
-        str(convert_values(survpt[1], doc)),
-        str(convert_values(survpt[2], doc)),
-        str(convert_units(surveydistance, document)), surveyptangle],
+            points_distances_angles(survpt)[0],
+            points_distances_angles(survpt)[1],
+            points_distances_angles(survpt)[2],
+            points_distances_angles(survpt)[3],
+            points_distances_angles(survpt)[4]],
         ['Project Base Point to Survey Delta X', ' ', ' ', ' ',
-        str(convert_units(calculate_delta_x(
-            baspt, survpt), document))],
+            convert_units(calculate_delta_x(
+                baspt, survpt), document)],
         ['Project Base Point to Survey Delta Y', ' ', ' ', ' ',
-        str(convert_units(calculate_delta_y(
-            baspt, survpt), document))],
+            convert_units(calculate_delta_y(
+                baspt, survpt), document)],
         ['Planar Distance between Base Point and Survey Point',
         ' ', ' ', ' ', 
-        str(convert_units(calculate_horizontal_distance(
-            baspt, survpt), document)),
-            truenorthangle],
+            convert_units(calculate_horizontal_distance(
+                baspt, survpt), document),
+                truenorthangle],
         ['Total Distance between Base Point and Survey Point',
         ' ', ' ', ' ',
-        str(convert_units(calculate_distance(
-            baspt, survpt), document)),
+            convert_units(calculate_distance(
+                baspt, survpt), document),
             ' '],
         ['Project Elevation', 
-        ' ', ' ', str(convert_values(baspt[2], doc)),
-        str(convert_units((survpt[2] - baspt[2]), document))]]
+            ' ', ' ', convert_values(baspt[2], document),
+            convert_units((survpt[2] - baspt[2]), document)]]
     # Print Table
     output.print_table(table_data=tbdata, 
                     title='Project Coordinates and Distances',
@@ -421,12 +449,12 @@ def check_model_extents(document=doc):
     Args:
         doc (Document): The current document.
     Returns:
-        None
+        Test Score (int): The score of the test.
     """
         # _______________________________________Get the bounding box of the 3D view
     print("")
     output.print_md('# Checking the extents of the 3D view bounding box')
-    bbox = get_temp_view_bbox(doc)
+    bbox = get_temp_view_bbox(document)
     min = (bbox.Min.X, bbox.Min.Y, bbox.Min.Z)
     max = (bbox.Max.X, bbox.Max.Y, bbox.Max.Z)
     print("")
@@ -435,9 +463,10 @@ def check_model_extents(document=doc):
     if (calculate_distance(min, INTERNAL_ORIGIN) > EXTENT_DISTANCE or 
         calculate_distance(max, INTERNAL_ORIGIN) > EXTENT_DISTANCE):
         output.print_md('{} 3D View Bounding Box extends more than {}'.format(BAD_STRG, CRITERIA_STRG))
+        return 0
     else:
         output.print_md('{} 3D View Bounding Box is located less than {}'.format(GOOD_STRG, CRITERIA_STRG))
-        test_score = 1
+        return 1
 
 def check_design_options(document=doc):
     """
@@ -445,7 +474,7 @@ def check_design_options(document=doc):
     Args:
         doc (Document): The current document.
     Returns:
-        None
+        Test Score (int): The score of the test.
     """
     
         # _____________________________________________Get Objects in Design Options
@@ -485,55 +514,50 @@ def check_design_options(document=doc):
             output.print_md('{} Manual investigation is required'.format(WARN_STRG))
         counter = 0
         limit = 10
+        violating_design_option_objects = violating_design_option_objects[:limit]
         for x in violating_design_option_objects:
-            if counter > limit:
-                break
-            else:
-                    setid = violating_option_sets[counter]
-                    print(output.linkify(x.Id) + 
-                            "   " +
-                            str(x.Name) +
-                            " - Is part of " +
-                            str(doc.GetElement(setid).Name) +
-                            " - " +
-                            str(x.DesignOption.Name)
-                            )
-                    counter += 1
-        test_score = 0
+            setid = violating_option_sets[counter]
+            print(output.linkify(x.Id) + 
+                    "   " +
+                    str(x.Name) +
+                    " - Is part of " +
+                    str(doc.GetElement(setid).Name) +
+                    " - " +
+                    str(x.DesignOption.Name)
+                    )
+            counter += 1
+        return 0
     else:
         output.print_md('{} No object in any design option is located more than {}'.format(GOOD_STRG, CRITERIA_STRG))
-        test_score = 1
-    # __________________________________________________________Check Test Score
-    if test_score >= 2:
-        output.print_md('{} All Tests Passed.'.format(GOOD_STRG))
-        script.exit()
-    else:
-        output.print_md( 
-        '{} Distant objects detected, Proceeding with additional analysis'.format(BAD_STRG))
+        return 1
 
 def check_linked_elements(document=doc):
-
+    """
+    Checks the extents of the CAD and RVT links in the model.
+    Args:
+        doc (Document): The current document.
+    Returns:
+        Test Score (int): The score of the test.
+    """
+    limit = 5
     print(divider)
     output.print_md('# Checking the extents of the CAD and RVT links')
     print(divider)
     badcads = get_far_elements(doc, cad=True, rvt=False)
+    badcads = badcads[:limit]
     badrvts = get_far_elements(doc, cad=False, rvt=True)
-    counter = 0
-    limit = 5
-    if len(badcads) > 0 or len(badrvts) > 0:
+    badrvts = badrvts[:limit]
+
+    if badcads:
         for x in badcads:
             print(output.linkify(x.Id)+"__" + 
                 str(x.Name) + '  ' + str(x.Category.Name))
-            if counter == limit:
-                break
-            counter += 1
-        counter = 0
+
+    if badrvts:
         for x in badrvts:
             print(output.linkify(x.Id)+"__" + 
                 str(x.Name) + '  ' + str(x.Category.Name))
-            if counter == limit:
-                break
-            counter += 1
+
     else:
         output.print_md('{} All CAD and RVT Links are located less than {}'.format(GOOD_STRG, CRITERIA_STRG))
         test_score = 2
@@ -551,7 +575,7 @@ def check_linked_elements(document=doc):
     output.print_md('# Checking everything, It is going to take a while.')
     output.print_md('# Please be patient.')
 
-def check_all_elements(document=doc):
+def check_all_elements(document=doc): # Main Function
     """
     Checks the extents of all elements in the model.
     Args:
@@ -560,11 +584,10 @@ def check_all_elements(document=doc):
         None
         """
     print(divider)
-
-    badelements = get_far_elements(document,False, False, True)                                          
-    counter = 0
+    badelements = get_far_elements(document,False, False)                                          
     limit = 10
-    if len(badelements) > 0:
+    badelements = badelements[:limit]
+    if badelements:
         if len(badelements) > limit:
             output.print_md('{} Showing the first 10 objects'.format(WARN_STRG))
             output.print_md('{} Manual investigation is required'.format(WARN_STRG))
@@ -572,12 +595,9 @@ def check_all_elements(document=doc):
         for x in badelements:
             print(output.linkify(x.Id)+ '  ' + 
                     str(x.Name) + '  ' + str(x.Category.Name))
-            if counter == limit:
-                break
-            counter += 1
+
     else:
         output.print_md('{} All Objects are located less than {}'.format(GOOD_STRG, CRITERIA_STRG))
-        test_score += 1
     output.print_md('# All tests completed, Review the results above.')
 # endregion
 # region Preflight Test Case
@@ -611,10 +631,19 @@ class ModelChecker(PreflightTestCase):
     def startTest(self, doc, output):
         with revit.DryTransaction("Checking Model Extents"):
             check_model_coordinates(doc)
-            check_model_extents(doc)
-            check_design_options(doc)
-            check_linked_elements(doc)
-            check_all_elements(doc)
+            task_1 = check_model_extents(doc)
+            task_2 = check_design_options(doc)
+            if task_1 + task_2 < 2:
+                output.print_md(
+                    ('{} 3D View Bounding Box extends more than {}'.
+                     format(BAD_STRG, CRITERIA_STRG)))
+                check_linked_elements(doc)
+                check_all_elements(doc)
+            else:
+                output.print_md(
+                    ('{} 3D View Bounding Box is located less than {}'.
+                     format(GOOD_STRG, CRITERIA_STRG)))
+
 
     def tearDown(self, doc, output):
         pass
